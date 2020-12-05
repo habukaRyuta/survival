abe42de307dd44c33eb6d27ebf95697157cad508
Ryu <ryuta.h1012@gmail.com>
2020-12-05 16:20:22 +0900

    プロジェクト作成

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //移動方向
+    [SerializeField] private Vector3 velocity;
+
+    //移動速度
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    void Update()
+    {
+        velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity.z += 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity.z -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x -= 1;
+        }
+
+        //いずれの方向に移動している場合
+        if (velocity.magnitude > 0)
+        {
+            transform.position += velocity;
+        }
+    }
+}
